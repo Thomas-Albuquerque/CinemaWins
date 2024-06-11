6874d49eac5422be17c4b6281a2baaa07dfd6a73
Thomas-Albuquerque <thomas.albuquerque@sptech.school>
2024-06-11 00:08:35 -0300

    script do banco atualizado

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -22,7 +22,9 @@ fkQuiz INT,
 CONSTRAINT fkUsuarioResposta FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
 CONSTRAINT fkQuizResposta FOREIGN KEY (fkQuiz) REFERENCES quiz(idQuiz),
 PRIMARY KEY (idResposta, fkUsuario, fkQuiz),
-pontuacao INT
+pontuacao INT,
+acertos INT,
+erros INT
 );
 
 CREATE TABLE review (
@@ -31,9 +33,17 @@ CREATE TABLE review (
     foto VARCHAR(256),
     titulo VARCHAR(100),
     descricao VARCHAR(263),
-    qtd_curtidas INT,
     data_publicacao DATETIME,
     CONSTRAINT fkUsuarioReview FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
+);
+
+
+CREATE TABLE curtidas (
+idCurtida INT PRIMARY KEY AUTO_INCREMENT,
+fkUsuario INT,
+fkReview INT,
+CONSTRAINT fkUsuarioCurtida FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
+CONSTRAINT fkReviewCurtida FOREIGN KEY (fkReview) REFERENCES review(idReview)
 );
 
 SELECT * FROM usuario;
@@ -81,25 +91,7 @@ JOIN resposta ON usuario.id = resposta.fkUsuario
 GROUP BY usuario.id, usuario.nome, usuario.email
 ORDER BY total_pontuacao DESC;
 
-INSERT INTO review VALUES (DEFAULT, 3 , null, 'Meu filme favorito é Crespusculo, edai?', 'nem todos os filmes precisam ter um história super profunda',
- 20, '2024-06-03 14:19:01');
- 
- ALTER TABLE resposta 
-ADD COLUMN acertos INT,
-ADD COLUMN erros INT;
 
-ALTER TABLE review
-ADD COLUMN qtd_curtidas INT DEFAULT 0;
 
--- Criar a tabela 'curtidas'
--- CREATE TABLE curtidas (
---    idCurtida INT PRIMARY KEY AUTO_INCREMENT,
---    fkUsuario INT,
---    fkReview INT,
---    CONSTRAINT fkUsuarioCurtida FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
---    CONSTRAINT fkReviewCurtida FOREIGN KEY (fkReview) REFERENCES review(idReview)
--- );
-ALTER TABLE resposta
-MODIFY COLUMN pontuacao INT DEFAULT 0;
 
 SELECT * FROM curtidas;
